59a6aa338d8b2dc8d4d8f1a97b7892bf24711d43
Gustavo Soares <sirgustavo4@gmail.com>
2025-07-29 22:49:47 -0300

    feat: Criando repositorys suporte ao Ef core e ao IMemoryCache

---
+++ Psalms.AspNetCore.Auth.Jwt/Repository/RefreshToken/EF/RefreshTokenRepositoryEF.cs
@@ -0,0 +1,23 @@
+﻿using Psalms.AspNetCore.Auth.Jwt.Models;
+using Psalms.AspNetCore.Auth.Jwt.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Psalms.AspNetCore.Auth.Jwt.Repository.RefreshToken;
+
+internal class EFRefreshTokenRepository(IPsalmsRefreshTokenEFContext context) : IPsalmsRefreshTokenRepository
+{
+    public async Task DeleteRefreshTokenAsync(string refreshToken)
+    {
+        context.Refreshes.Remove(await context.Refreshes.FirstAsync());
+        await context.ConfirmChangesAsync();
+    }
+
+    public async Task<bool> RefreshTokenExistAsync(string refreshToken)
+        => await context.Refreshes.AnyAsync(x => x.RefreshToken == refreshToken);
+
+    public async Task SaveRefreshTokenAsync(RefreshTokenModel model)
+    {
+        await context.Refreshes.AddAsync(model);
+        await context.ConfirmChangesAsync();
+    }
+}
