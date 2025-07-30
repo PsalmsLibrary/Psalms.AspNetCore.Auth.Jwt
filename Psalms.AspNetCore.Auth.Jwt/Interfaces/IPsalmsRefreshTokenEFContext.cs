62e32a985aceefe9afcb32ddd2a808785ec81468
Gustavo Soares <sirgustavo4@gmail.com>
2025-07-29 22:51:46 -0300

    refactor: Atualizando a extensão

---
+++ Psalms.AspNetCore.Auth.Jwt/Interfaces/IPsalmsRefreshTokenEFContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Psalms.AspNetCore.Auth.Jwt.Models;
+
+namespace Psalms.AspNetCore.Auth.Jwt.Interfaces;
+    
+public interface IPsalmsRefreshTokenEFContext
+{
+    DbSet<RefreshTokenModel> Refreshes { get; set; }
+    Task ConfirmChangesAsync();
+}
